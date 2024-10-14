2740a041e9223c96ae510255ef717a577351a00e
sathyabanu <sathyahemanth2218@outlook.com>
2024-10-14 15:45:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime;
+
+namespace ques1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sample Input:");
+            Console.WriteLine("Enter an integer value");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the float value");
+            float b=float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the double value");
+            double c=double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the long value");
+            long d=long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the string");
+            string e = Console.ReadLine();
+            Console.WriteLine("Sample Output");
+            Console.WriteLine("int:"+a);
+            Console.WriteLine("float:"+b);
+            Console.WriteLine("double:"+c);
+            Console.WriteLine("long:"+d);
+            Console.WriteLine("string:"+e);
+            
+        }
+    }
+}
